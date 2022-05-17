1a3dbd60e894954df0df99395185316b97d0296f
Murilo Gimenez <91744500+murilouwu@users.noreply.github.com>
2022-05-16 21:05:42 -0300

    0.1

--- jogos.php
+++ jogos.php
@@ -7,7 +7,7 @@
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 		<link rel="stylesheet" type="text/css" href="estilo/estile.css">
 		<script src="java/fun.js"></script>
-		<link rel="shortcut icon" href="img/face.png">
+		<link rel="shortcut icon" href="img/play.png">
 		<title>Play quiz</title>
 	</head>
 	<body>
@@ -29,6 +29,29 @@
 			        </div>
 		        </div>
 		    </nav>
+		    <div class="position-relative overflow-hidden p-3 p-md-5 text-center bg-dark bg-opacity-50">
+	            <div class="col-md-5 lg-5 mx-auto my-2">
+	                <img src="img/logo.png" class="imglink">
+	                <p class="lead fw-normal text-light">
+	                    welcome to the quiz game
+	                </p>
+	            </div>
+	        </div>
+	        <div class="container d-flex flex-column justify-content-center">
+	        	<div class="d-flex flex-column justify-content-center text-center align-items-center p-md-3">
+	        		<h1 class="fw-normal text-light">Your quiz:</h1>
+	        		<a class="nav-link text-warning bg-dark rounded-pill" style="width: 30%;" href="myquiz.php">To see</a>
+	        		<h1 class="fw-normal text-light p-md-3">Quiz:</h1>
+	        	</div>
+	        	<div class="d-flex flex-row justify-content-center text-center align-items-center p-md-1">
+	        		<div class="collumn bg-dark rounded-3 p-md-2" style="margin-right: 1vh;">
+	        			<h2 class="text-light">name 1</h2>
+	        			<a class="nav-link text-light linkcss" href="#"><strong>Play</strong></a>
+	        			<h6 class="text-light">categoria: matematica</h6>
+	        			<h6 class="text-light">peruntas: 10</h6>
+	        		</div>
+	        	</div>
+	        </div>
 		</div>
 	</body>
 </html>
--- myquiz.php
+++ myquiz.php
@@ -6,10 +6,9 @@
 		<link href="https:\\cdn.jsdelivr.net\npm\bootstrap@5.1.3\dist\css\bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 		<link rel="stylesheet" type="text/css" href="estilo/estile.css">
-		<link rel="stylesheet" type="text/css" href="estilo/estileap.css">
 		<script src="java/fun.js"></script>
-		<link rel="shortcut icon" href="img/rank.png">
-		<title>Rank</title>
+		<link rel="shortcut icon" href="img/logo.png">
+		<title>My Quiz</title>
 	</head>
 	<body>
 		<div class="all">
@@ -24,28 +23,15 @@
 			                	<a class="nav-link text-warning linkcss" aria-current="page" href="pageUser.php"><strong>Perfil</strong></a>
 			                </li>
 			                <li class="nav-item">
+			                	<a class="nav-link text-warning linkcss" aria-current="page" href="rank.php"><strong>Ranking</strong></a>
+			                </li>
+			                <li class="nav-item">
 			                	<a class="nav-link text-warning linkcss" aria-current="page" href="jogos.php"><strong>Play</strong></a>
 			                </li>
 			        	</ul>
 			        </div>
 		        </div>
 		    </nav>
-		    <div class="container d-flex flex-column justify-content-center">
-		    	<div class="d-flex flex-column justify-content-center text-center align-items-center p-md-3">
-		    		<h1 class="fw-normal text-light">Your Rank:</h1>
-		    		<div class="collumn container-fluid bg-dark rounded-pill p-md-2 border border-warning border-3">
-	        			<h2 class="text-light">User: Paulo</h2>
-	        			<h6 class="text-light">Position: 800</h6>
-	        		</div>
-	        		<h1 class="fw-normal text-light p-md-3">World Rank:</h1>
-		    	</div>
-		    	<div class="d-flex flex-column justify-content-center text-center align-items-center p-md-1 overflow-auto" style="height: 50vh;">
-	        		<div class="container-fluid d-flex flex-row justify-content-around align-items-center bg-dark rounded-pill p-md-2 border border-warning border-3 text-center">
-	        			<h3 class="text-light">User: Carlos</h3>
-	        			<h6 class="text-light">Position: 1</h6>
-	        		</div><br>
-	        	</div>
-		    </div>
 		</div>
 	</body>
 </html>
