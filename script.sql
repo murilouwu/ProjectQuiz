b63b09c4488ea84da55521e2009f52d9dd1d1534
Murilo Gimenez <91744500+murilouwu@users.noreply.github.com>
2022-05-25 10:41:16 -0300

    1.0

--- script.sql
+++ script.sql
@@ -1,13 +1,36 @@
 CREATE DATABASE projeto;
 USE projeto;
 
-CREATE TABLE user(
-cd INT AUTO_INCREMENT PRIMARY KEY,
-nome VARCHAR(100) NOT NULL,
-email VARCHAR(100) NOT NULL,
-senha VARCHAR(20) NOT NULL,
-foto VARCHAR(120) NULL);
+CREATE TABLE user (
+cd INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(450),
+email VARCHAR(450),
+senha VARCHAR(200),
+foto VARCHAR(100),
+ponto INT,
+rank INT);
 
 CREATE TABLE categoria(
-cd INT AUTO_INCREMENT PRIMARY KEY,
-nome VARCHAR(200));
+cd INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(405));
+
+CREATE TABLE jogo(
+cd INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(450),
+id_usuario INT,
+id_categoria INT,
+FOREIGN KEY (id_usuario) REFERENCES user(cd),
+FOREIGN KEY (id_categoria) REFERENCES categoria(cd));
+
+CREATE TABLE pergunta(
+cd INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(200),
+id_jogo INT,
+FOREIGN KEY (id_jogo) REFERENCES jogo(cd));
+
+CREATE TABLE alternativa (
+cd INT PRIMARY KEY AUTO_INCREMENT,
+texto VARCHAR(200),
+resposta BOOLEAN,
+id_pergunta INT,
+FOREIGN KEY (id_pergunta) REFERENCES pergunta(cd));
